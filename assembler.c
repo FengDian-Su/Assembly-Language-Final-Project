173f76994acce76a18acbae4c5e088687582ea5e
NJ <129062942+FengDian-Su@users.noreply.github.com>
2023-05-27 00:25:53 +0800

    Update assembler.c

--- assembler.c
+++ assembler.c
@@ -130,27 +130,19 @@ int main(){
 			memset(output_line, 0, 50);
 			strcat(output_line, "\t");
 			strcat(output_line, str);
-//			printf("%s", output_line);
-//			fputs(output_line, im);
 			break;
 		}	
 		sprintf(locctr, "%x", locnum);
 		strcpy(output_line, locctr);
-		for (i = 0; i < 3; i++)
-		{
+		for (i = 0; i < 3; i++){
 			strcat(output_line, "\t");
 			strcat(output_line, field[i]);
 		}
 		strcat(output_line, "\n");
 		update_sym(field[0], locctr);
-//		printf("%s", output_line);
-//		fputs(output_line, im);
-		if (initial == 1)
-		{
-			if (str[0] != '.')
-			{
-				if (*field[0] != 0)		// There is a label
-				{
+		if (initial == 1){
+			if (str[0] != '.'){
+				if (*field[0] != 0){
 					if (is_sym(field[0]) == 1)
 						printf("Error Flag : Duplicate symbol !\n");
 					else if (is_sym(field[0]) == 0)
@@ -158,14 +150,12 @@ int main(){
 				}
 				if (search_opt(field[1]) != -1 || strcmp(field[1], "WORD") == 0)
 					locnum += 3;
-				else
-				{
+				else{
 					if (strcmp(field[1], "RESW") == 0)
 						locnum += 3*atoi(field[2]);
 					else if (strcmp(field[1], "RESB") == 0)
 						locnum += atoi(field[2]);
-					else if (strcmp(field[1], "BYTE") == 0)
-					{
+					else if (strcmp(field[1], "BYTE") == 0){
 						char operand_type = field[2][0];
 						if (operand_type == 'C')
 							locnum += (strlen(field[2])-3);
@@ -178,23 +168,16 @@ int main(){
 	}
 	fclose(fs);
 	memset(str, 0, 50);
-//	memset(output_line, 0, 50);
-	
-	//path 2
 	file_line = 46;
 	int p2_cnt = 0, total_;
 	char loc[file_line][50], f2[4][50];
 	im = fopen("immediate_file.txt", "r");
-	while (!feof(im))
-	{
+	while (!feof(im)){
 		int flag2 = 0, f_cnt = 0;
 		char opcode[5], final_object_code[10];
 		fgets(str, 50, im);
-//		printf("%s\n", str);
-		for (i = 0; i < strlen(str); i++)
-		{
-			if (str[i] == '\t' || str[i] == ' ' || str[i] == '\n')
-			{
+		for (i = 0; i < strlen(str); i++){
+			if (str[i] == '\t' || str[i] == ' ' || str[i] == '\n'){
 				f2[flag2][f_cnt] = '\0';
 				f_cnt = 0;
 				flag2++;
@@ -205,58 +188,36 @@ int main(){
 		printf("%s\t%s\t%s\t%s\t", f2[0], f2[1], f2[2], f2[3]);
 		int find_o = search_opt(f2[2]);
 		strcpy(opcode, opt_num[find_o]);
-//		printf("%s -> %s,\t", f2[2], opcode);
 		char trans_hex[10];
-		if (find_o != -1)
-		{
+		if (find_o != -1){
 			char find_operand[10];
-//			printf("f3 = %s.\n", f2[3]);
-			if (*f2[3] != 0)
-			{
+			if (*f2[3] != 0){
 				strcpy(find_operand, search_sym(f2[3]));
-				if (*find_operand == 0)
-				{
+				if (*find_operand == 0){
 					strcpy(find_operand, "0000");
 					printf("Error Flag : Undefined Symbol ");
 				}
-//				printf("%s -> %s.\n", f2[3], find_operand);
 			}
 			else
-			{
 				strcpy(find_operand, "0000");
-//				printf("%s -> %s.\n", f2[3], find_operand);
-			}
 			strcpy(final_object_code, opcode);
 			strcat(final_object_code, find_operand);
 			printf("%s\n", final_object_code);
 		}
-		else if (strcmp(f2[2], "BYTE") == 0)
-		{
-//			printf("** now line **\t%s\n", f2[3]);
+		else if (strcmp(f2[2], "BYTE") == 0){
 			if (f2[3][0] == 'C')
-			{
-				for (i = 2; f2[3][i] != '\''; i++)
-				{
-					
+				for (i = 2; f2[3][i] != '\''; i++){
 					sprintf(trans_hex, "%x", f2[3][i]);
-//					printf("_%s ", trans_hex);
 					strcat(final_object_code, trans_hex);
 				}
-			}
-			else if (f2[3][0] == 'X')
-			{
+			else if (f2[3][0] == 'X'){
 				int ttc = 0;
 				for (i = 2; f2[3][i] != '\''; i++)
-				{
-//					printf("_%d ", f2[3][i]);
 					final_object_code[ttc++] = f2[3][i];
-				}	
 			}
 			printf("%s\n", final_object_code);
 		}
-		else if (strcmp(f2[2], "WORD") == 0)
-		{
-//			printf("** f2[3] = %s ** ", f2[3]);
+		else if (strcmp(f2[2], "WORD") == 0){
 			sprintf(trans_hex, "%x", atoi(f2[3]));
 			strcpy(final_object_code, trans_hex);
 			printf("%s\n", final_object_code);
@@ -266,9 +227,6 @@ int main(){
 		printf("\n");
 		memset(final_object_code, 0, 50);
 	}
-	printf("====================\n");
-	output_sym();
-	printf("====================\n");
 	fclose(im);
 	return 0;
 }
